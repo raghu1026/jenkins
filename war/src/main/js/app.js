3a0325b3e5c7b10294ee0bf68dbb2dbee012b16e
raghu1026 <123620701+raghu1026@users.noreply.github.com>
2023-02-05 11:43:25 +0530

    Delete war directory

--- war/src/main/js/app.js
+++
@@ -1,7 +0,0 @@
-import Notifications from "@/components/notifications";
-import SearchBar from "@/components/search-bar";
-import Tooltips from "@/components/tooltips";
-
-Notifications.init();
-SearchBar.init();
-Tooltips.init();
